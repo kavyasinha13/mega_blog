873a45023b86a2600ab628cd54f20fe1b7925a56
Kavya <kavya01092003@gmail.com>
2025-06-09 14:17:45 +0530

    blog project added

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import './styles.css';
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <div className='mt-12'>
+          <Outlet />
+          </div>
+        </main>
+        <div className='mt-12'>
+        <Footer />
+        </div>
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import { Container, PostCard } from '../components'
+import appwriteService from "../appwrite/config";
+
+function AllPosts() {
+    const [posts, setPosts] = useState([])
+    
+    useEffect(() => {
+        appwriteService.getPosts().then((response) => {
+          if (response) {
+            setPosts(response.documents); // Make sure the data structure matches
+          }
+        });
+      }, []);
+
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
+            </div>
+            </Container>
+    </div>
+  )
+}
+
+export default AllPosts
